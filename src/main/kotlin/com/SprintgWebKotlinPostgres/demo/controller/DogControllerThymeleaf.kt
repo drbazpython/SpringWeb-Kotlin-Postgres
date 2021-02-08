b2ebde4c3ceee64e68985763c849e729d4e68e08
Dr Barrie Smith <drbazpython@gmail.com>
2021-02-08 16:32:10 +0000

    Commit 5 - added Thymeleaf form template

--- src/main/kotlin/com/SprintgWebKotlinPostgres/demo/controller/DogControllerThymeleaf.kt
+++ src/main/kotlin/com/SprintgWebKotlinPostgres/demo/controller/DogControllerThymeleaf.kt
@@ -1,20 +1,37 @@
 package com.SprintgWebKotlinPostgres.demo.controller
-
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import com.SprintgWebKotlinPostgres.demo.model.Dog
 import com.SprintgWebKotlinPostgres.demo.repository.DogRepsoitory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+
 @Controller
 class DogControllerThymeleaf {
 	@Autowired
 	lateinit var dogRepository: DogRepsoitory
+	private val log = LoggerFactory.getLogger(DogControllerThymeleaf::class.java)
 
 	@GetMapping("/dogs/list")
-	fun listDogsThymeleaf(model: Model):String{
+	fun listDogs(model: Model):String{
 		model.addAttribute("dogs",dogRepository.findAll())
 		return "list"
-
+	}
+	@GetMapping("/create")
+	fun createDogForm(model:Model):String{
+		model.addAttribute("dog", Dog())
+		return "create"
+	}
+	@PostMapping("/create")
+	fun saveDog(@ModelAttribute("dog") dog:Dog, model:Model):String{
+		model.addAttribute("dog", dog)
+		val info = String.format("Submission: name = %s", dog.name)
+		log.info(info)
+		dogRepository.save(dog)
+		return "result"
 	}
 }
