e9c1a38f8df80668c095fcdf16076cc86472b015
Dr Barrie Smith <drbazpython@gmail.com>
2021-02-08 14:31:49 +0000

    Commit 4 - added data to initialise database with 2 dogs

--- src/main/kotlin/com/SprintgWebKotlinPostgres/demo/DemoApplication.kt
+++ src/main/kotlin/com/SprintgWebKotlinPostgres/demo/DemoApplication.kt
@@ -1,19 +1,32 @@
 package com.SprintgWebKotlinPostgres.demo
 
+import com.SprintgWebKotlinPostgres.demo.model.Dog
+import com.SprintgWebKotlinPostgres.demo.repository.DogRepsoitory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
-@RestController
-class DemoApplication {
-	@GetMapping("/HelloWorld")
-	fun helloworld():String{
-		return "Hello, World!"
+
+class DemoApplication (val dogRepository: DogRepsoitory): ApplicationRunner{
+	override fun run(args:ApplicationArguments){
+
+		val dog1 = Dog (9999,"Meg",9,"B")
+		val dog2 = Dog (9999,"Duffy",5,"B")
+		dogRepository.save(dog1)
+		dogRepository.save(dog2)
 	}
+
 }
+
 	fun main(args: Array<String>) {
 		runApplication<DemoApplication>(*args)
 	}
+
 
